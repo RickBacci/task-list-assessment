ec0f3518d960c64f22ed3e72ac266e1d0b5e9879
Rick Bacci <rickbacci@gmail.com>
2016-01-20 14:06:15 -0700

    add edit task list spec

--- spec/features/user_can_manage_task_lists_spec.rb
+++ spec/features/user_can_manage_task_lists_spec.rb
@@ -24,5 +24,40 @@ RSpec.feature "User", type: :feature do
 
     expect(page).to have_content('TaskList successfully created')
   end
+
+  it 'can edit their own tasklist' do
+    User.create(email: 'user@email.com', password: 'password')
+
+    visit root_path
+
+    click_link "Login"
+    fill_in "Email", with: 'user@email.com'
+    fill_in 'Password', with: 'password'
+    click_link_or_button "Login"
+    click_link 'New TaskList'
+
+    fill_in "Title", with: 'test title'
+    fill_in 'Description', with: 'description'
+
+    click_link_or_button "Create TaskList"
+
+    expect(page).to have_content('TaskList successfully created')
+    expect(page).to have_content('test title')
+    expect(page).to have_content('description')
+
+
+
+
+    # click_link_or_button "Edit"
+    #
+    # fill_in "Title", with: 'edited title'
+    # fill_in 'Description', with: 'edited description'
+    #
+    # click_link_or_button "Update TaskList"
+    #
+    # expect(page).to have_content('TaskList successfully updated!')
+    # expect(page).to have_content('edited title')
+    # expect(page).to have_content('edited description')
+  end
 end
 
