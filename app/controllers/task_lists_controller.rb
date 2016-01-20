51b193e6ae6db0874c8a20b58a6a09e90f26c48c
Rick Bacci <rickbacci@gmail.com>
2016-01-20 13:02:07 -0700

    add new action to task_lists controller
    - rename variables to a more logical name

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -3,9 +3,25 @@ class TaskListsController < ApplicationController
   end
 
   def new
-    @tasklist = TaskList.new
+    @task_list = TaskList.new
   end
 
   def create
+    @task_list = current_user.task_lists.new(tasklist_params)
+
+    if @task_list
+      flash[:success] = "TaskList successfully created"
+      redirect_to tasks_path
+    else
+      flash[:failure] = "There was a problem creating your TaskList"
+      render :new
+    end
   end
+
+  private
+
+  def tasklist_params
+    params.require(:task_list).permit(:new, :create)
+  end
+
 end
