d92505dcb6a26424c3a738ede3ee5c3d64f21c26
Rick Bacci <rickbacci@gmail.com>
2016-01-20 17:14:16 -0700

    add create_tasklist helper

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,10 +1,20 @@
 
 def sign_in
-    visit tasks_path
+  visit tasks_path
 
-    click_link "Sign Up"
-    fill_in "Email", with: 'user@email.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Confirmation', with: 'password'
-    click_link_or_button "Create Account"
+  click_link "Sign Up"
+  fill_in "Email", with: 'user@email.com'
+  fill_in 'Password', with: 'password'
+  fill_in 'Confirmation', with: 'password'
+  click_link_or_button "Create Account"
 end
+
+def create_tasklist
+  click_link 'New TaskList'
+
+  fill_in "Title", with: 'title'
+  fill_in 'Description', with: 'description'
+
+  click_link_or_button "Create TaskList"
+end
+
