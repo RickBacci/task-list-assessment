54dd1ae04f0c4f225f805f8e9ba0557b634d74f4
Rick Bacci <rickbacci@gmail.com>
2016-01-21 00:09:00 -0700

    fix new task form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,9 +5,25 @@ class TasksController < ApplicationController
   end
 
   def new
+    # @task_list = current_user.task_lists.find(params[:id])
     @task_list = current_user.task_lists.find(params[:task_list_id])
     @task = Task.new
   end
+
+  def create
+    # task_list = current_user.task_lists.where(id: params[:task_list])
+    @task_list = current_user.task_lists.find(params[:id])
+    require 'pry'; binding.pry
+    @task = Task.create(task_params)
+    # task_list << @task.save
+
+    if @task.save
+      flash[:success] = "TaskList successfully created"
+      redirect_to task_list_tasks_path(task_list.id)
+    else
+      flash[:failure] = "There was a problem creating your TaskList"
+      render :new
+    end
   end
 
   private
