d14e751e955eb0ac2b05863aeca7a222a9c18570
Rick Bacci <rickbacci@gmail.com>
2016-01-20 18:28:17 -0700

    add new action to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,18 @@
 class TasksController < ApplicationController
+
   def index
     @task_lists = current_user.task_lists if current_user
   end
+
+  def new
+    @task_list = current_user.task_lists.where(id: params[:task_list])
+    @task      = Task.new
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:status, :notes, :start, :due)
+  end
+
 end
