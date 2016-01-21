64b0272bf5045639bce366018bd20a55b03fcebf
Rick Bacci <rickbacci@gmail.com>
2016-01-21 01:04:00 -0700

    check for existence of start and due in Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,13 +10,13 @@ class Task < ActiveRecord::Base
 
 
   def start_date_cannot_be_in_the_past
-    if start < Date.today
+    if start.present? && start < Date.today
       errors.add(:start, "can't be in the past")
     end
   end
 
   def due_date_cannot_be_in_the_past
-    if due < Date.today
+    if due.present? && due < Date.today
       errors.add(:due, "can't be in the past")
     end
   end
